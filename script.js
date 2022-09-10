88834b7e609befb6e94a86ad8d4c3f280ca3b145
Elisafa <elisafajunior@gmail.com>
2022-09-10 10:42:18 -0300

    realizado javascript da rolagem

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ btnNavEl.addEventListener('click', function(){
   headerEl.classList.toggle("nav-open");
 });
 
+//smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach(function(link){
+  link.addEventListener('click', function(e){
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    //scroll back to top
+    if(href === "#") window.scrollTo({
+      top:0,
+      behavior: 'smooth'
+    });
+    if(href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({behavior: "smooth"});
+    };
+    if(link.classList.contains('main-nav-link')){
+      headerEl.classList.toggle("nav-open");
+    };
+  });
+});
 
 
 ///////////////////////////////////////////////////////////
